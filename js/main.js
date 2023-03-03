23ac75e27609a7a9b93f30241d5a126624a4b182
erik-gustafsson-nti-johanneberg <erik.gustafsson4@elev.ga.ntig.se>
2023-03-03 17:31:37 +0100

    v1.2.1
    - En liten uppgradering på bildspelet, lade till punkter och pil knappar, dock fungerar inte knapparna ännu.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,7 @@
 window.onload = function() {
-    let currentImageIndex = 0;
-    const slideshowImages = document.querySelectorAll('.start-slideshow-img');
 
-    // Hide all images except for the first one
-    for (let i = 1; i < slideshowImages.length; i++) {
-        slideshowImages[i].style.display = 'none';
-    }
-
-    // Set an interval timer to change the image every few seconds
-    setInterval(() => {
-        // Hide the current image
-        slideshowImages[currentImageIndex].style.display = 'none';
-
-        // Increment the current image index
-        currentImageIndex++;
-
-        // If the current index is equal to the number of images, reset it to zero
-        if (currentImageIndex >= slideshowImages.length) {
-            currentImageIndex = 0;
-        }
-
-        // Display the next image
-        slideshowImages[currentImageIndex].style.display = 'block';
-    }, 3000);
+    
+    
 }
 
 
@@ -30,6 +9,7 @@ window.onload = function() {
 
 
 document.addEventListener("DOMContentLoaded", () => {
+    // --- The navigation --- //
     let button = document.querySelector("i");
     let menu = document.querySelector("nav");
     let layer = document.querySelector(".fade-layer");
@@ -42,5 +22,43 @@ document.addEventListener("DOMContentLoaded", () => {
         menu.classList.toggle("show");
         layer.classList.toggle("visible");
     }
+
+
+    // --- The slideshow --- //
+    let currentImageIndex = 0;
+    const slideshowImages = document.querySelectorAll('.start-slideshow-img');
+    const dots = document.querySelectorAll('.dot');
+
+    // Hide all images except for the first one
+    for (let i = 1; i < slideshowImages.length; i++) {
+        slideshowImages[i].style.display = 'none';
+    }
+
+    // Set the first dot to active
+    dots[currentImageIndex].classList.add('active');
+
+
+    // Set an interval timer to change the image every few seconds
+    setInterval(() => {
+        // Hide the current image
+        slideshowImages[currentImageIndex].style.display = 'none';
+        dots[currentImageIndex].classList.remove('active');
+
+        // Increment the current image index
+        currentImageIndex++;
+
+        // If the current index is equal to the number of images, reset it to zero
+        if (currentImageIndex >= slideshowImages.length) {
+            currentImageIndex = 0;
+        }
+
+        // Display the next image
+        slideshowImages[currentImageIndex].style.display = 'block'
+        dots[currentImageIndex].classList.add('active');
+    }, 3000);
+
+
+
 });
+
 
