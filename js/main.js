6a93b3b40378af980776d3f07336555eb13ad284
erik-gustafsson-nti-johanneberg <erik.gustafsson4@elev.ga.ntig.se>
2023-03-03 14:32:52 +0100

    v1.2
    - Fixade headern
    - Fixade bildspel
    - Fixade föreningssidan

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+window.onload = function() {
+    let currentImageIndex = 0;
+    const slideshowImages = document.querySelectorAll('.start-slideshow-img');
+
+    // Hide all images except for the first one
+    for (let i = 1; i < slideshowImages.length; i++) {
+        slideshowImages[i].style.display = 'none';
+    }
+
+    // Set an interval timer to change the image every few seconds
+    setInterval(() => {
+        // Hide the current image
+        slideshowImages[currentImageIndex].style.display = 'none';
+
+        // Increment the current image index
+        currentImageIndex++;
+
+        // If the current index is equal to the number of images, reset it to zero
+        if (currentImageIndex >= slideshowImages.length) {
+            currentImageIndex = 0;
+        }
+
+        // Display the next image
+        slideshowImages[currentImageIndex].style.display = 'block';
+    }, 3000);
+}
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     let button = document.querySelector("i");
     let menu = document.querySelector("nav");
@@ -12,3 +43,4 @@ document.addEventListener("DOMContentLoaded", () => {
         layer.classList.toggle("visible");
     }
 });
+
