60dea1dbbc1716616001ac642cf41f97b30466d9
ERIK <mrerikgg@gmail.com>
2023-03-06 11:47:36 +0100

    v1.2.2
    - Patchade slideshowen, nu funkar bilderna, dom är anpassade till containern och ändrar sig inte.
    Samt funkar knapparna nu, då stannar bildspelet i nuläget.
    - Sen fixa ja så navigations menyn droppar ner från headern nu, ser lite snyggare ut.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-window.onload = function() {
-
-    
-    
-}
-
-
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
     // --- The navigation --- //
     let button = document.querySelector("i");
@@ -26,6 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // --- The slideshow --- //
     let currentImageIndex = 0;
+    let slideshow_timer = true;
     const slideshowImages = document.querySelectorAll('.start-slideshow-img');
     const dots = document.querySelectorAll('.dot');
 
@@ -37,15 +28,13 @@ document.addEventListener("DOMContentLoaded", () => {
     // Set the first dot to active
     dots[currentImageIndex].classList.add('active');
 
-
-    // Set an interval timer to change the image every few seconds
-    setInterval(() => {
+    function switchSlideshowImage(incrament) {
         // Hide the current image
         slideshowImages[currentImageIndex].style.display = 'none';
         dots[currentImageIndex].classList.remove('active');
 
         // Increment the current image index
-        currentImageIndex++;
+        currentImageIndex += incrament;
 
         // If the current index is equal to the number of images, reset it to zero
         if (currentImageIndex >= slideshowImages.length) {
@@ -55,8 +44,37 @@ document.addEventListener("DOMContentLoaded", () => {
         // Display the next image
         slideshowImages[currentImageIndex].style.display = 'block'
         dots[currentImageIndex].classList.add('active');
+    }
+
+
+    // Set an interval timer to change the image every few seconds
+    setInterval(() => {
+        if (slideshow_timer) {
+            switchSlideshowImage(1);
+        }
     }, 3000);
 
+    // Set the query for the next or previous image buttons
+    let btnNext = document.querySelector('.next')
+    let btnPrev = document.querySelector('.prev')
+
+    btnNext.addEventListener("click", nextImg);
+    btnPrev.addEventListener("click", prevImg);
+
+
+    function nextImg() {
+        slideshow_timer = false;
+        switchSlideshowImage(1);
+    }
+
+    function prevImg() {
+        slideshow_timer = false;
+        if (currentImageIndex == 0) {
+            switchSlideshowImage(slideshowImages.length-1)
+        } else {
+            switchSlideshowImage(-1);
+        }
+    }
 
 
 });
